b327c8294351579946bfac6678980dd6c67719f4
Joel <joelkaltman@gmail.com>
2019-09-08 18:00:38 -0300

    Configurated to use p3d engine

--- TAM2C/TAM2C.cpp
+++ TAM2C/TAM2C.cpp
@@ -4,6 +4,11 @@
 #include <GUI/Include/ApPanels.h>
 #include <GUI/Include/JtanPanels.h>
 
+#include <p3d/Include/P3D.h>
+#include <resources/Include/Admin.h>
+
+#pragma comment(lib, "Ws2_32.lib")
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
@@ -12,6 +17,8 @@ int main(int argc, char *argv[])
 
 	JtanPanels w2;
 	w2.show();
+
+	p3d::P3D::initialize(p3d::P3D::NetworkingMode::ONLY_LOCAL, p3d::P3D::Platform::DIRECTX_11);
 
 	return a.exec();
 }
