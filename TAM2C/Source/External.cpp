d1c4eaac4aa8aab4f54e7925caa44150674d4467
Joel <joelkaltman@gmail.com>
2019-11-04 21:07:46 -0300

    Configured for release

---
+++ TAM2C/Source/External.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+FILE _iob[] = { *stdin, *stdout, *stderr };
+
+extern "C" FILE * __cdecl __iob_func(void)
+{
+	return _iob;
+}
+
+#pragma comment(lib, "Ws2_32.lib")
--- testMonitor/testMonitor.cpp
+++ testMonitor/testMonitor.cpp
@@ -14,7 +14,11 @@
 #include <TAM2C/Include/Scene.h>
 
 FILE _iob[] = { *stdin, *stdout, *stderr };
-extern "C" FILE * __cdecl __iob_func(void) { return _iob; }
+
+extern "C" FILE * __cdecl __iob_func(void)
+{
+	return _iob;
+}
 
 #pragma comment(lib, "Ws2_32.lib")
 
